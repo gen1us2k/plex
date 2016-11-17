4de283e221ad4a0ea2573f99cf78777d96efe28c
Gen1us2k <minkin.andrew@gmail.com>
2016-11-17 11:06:20 +0600

    Done these methods:  * Get all playlists from Plex Media Server  * Get playlistInfo from Plex Media Server

--- models.go
+++ models.go
@@ -18,8 +18,113 @@ type (
 		Owned             string `xml:"owned,attr"`
 		Synced            string `xml:"synced,attr"`
 	}
+	PMSPlaylist struct {
+		RatingKey    string `xml:"ratingKey,attr"`
+		Key          string `xml:"key,attr"`
+		Guid         string `xml:"guid,attr"`
+		Type         string `xml:"type,attr"`
+		Title        string `xml:"title,attr"`
+		Summary      string `xml:"summary,attr"`
+		Smart        string `xml:"smart,attr"`
+		PlaylistType string `xml:"playlistType,attr"`
+		Composite    string `xml:"composite,attr"`
+		ViewCount    string `xml:"viewCount,attr"`
+		LastViewed   string `xml:"lastViewedAt,attr"`
+		Duration     string `xml:"duration,attr"`
+		LeafCount    string `xml:"leafCount,attr"`
+		Added        string `xml:"addedAt,attr"`
+		Updated      string `xml:"updatedAt,attr"`
+	}
+	PMSPlaylistInfo struct {
+		XMLName xml.Name `xml:"MediaContainer"`
+	}
 	ServersResponse struct {
 		XMLName xml.Name    `xml:"MediaContainer"`
 		Servers []PMSServer `xml:"Server"`
+	}
+	Metadata struct {
+		Video struct {
+			RatingKey            string `json:"ratingKey" xml:"ratingKey,attr"`
+			Key                  string `json:"key" xml:"key,attr"`
+			GrandparentTitle     string `json:"grandparentTitle" xml:"grandparentTitle,attr"`
+			GrandparentRatingKey string `json:"grandparentRatingKey" xml:"grandparentRatingKey,attr"`
+			ParentRatingKey      string `json:"parentRatingKey" xml:"parentRatingKey,attr"`
+			ParentYear           string `json:"parentYear" xml:"parentYear,attr"`
+			ParentTitle          string `json:"parentTitle" xml:"parentTitle,attr"`
+			GUID                 string `json:"guid" xml:"guid,attr"`
+			LibrarySectionID     string `json:"librarySectionID" xml:"librarySectionID,attr"`
+			Type                 string `json:"type" xml:"type,attr"`
+			Title                string `json:"title" xml:"title,attr"`
+			Summary              string `json:"summary" xml:"summary,attr"`
+			ViewCount            string `json:"viewCount" xml:"viewCount,attr"`
+			LastViewedAt         string `json:"lastViewedAt" xml:"lastViewedAt,attr"`
+			Year                 string `json:"year" xml:"year,attr"`
+			Thumb                string `json:"thumb" xml:"thumb,attr"`
+			Art                  string `json:"art" xml:"art,attr"`
+			Duration             string `json:"duration" xml:"duration,attr"`
+			AddedAt              string `json:"addedAt" xml:"addedAt,attr"`
+			UpdatedAt            string `json:"updatedAt" xml:"updatedAt,attr"`
+			ChapterSource        string `json:"chapterSource" xml:"chapterSource,attr"`
+			Media                struct {
+				VideoResolution string `json:"videoResolution" xml:"videoResolution,attr"`
+				ID              string `json:"id" xml:"id,attr"`
+				Duration        string `json:"duration" xml:"duration,attr"`
+				Bitrate         string `json:"bitrate" xml:"bitrate,attr"`
+				Width           string `json:"width" xml:"width,attr"`
+				Height          string `json:"height" xml:"height,attr"`
+				AspectRatio     string `json:"aspectRatio" xml:"aspectRatio,attr"`
+				AudioChannels   string `json:"audioChannels" xml:"audioChannels,attr"`
+				AudioCodec      string `json:"audioCodec" xml:"audioCodec,attr"`
+				VideoCodec      string `json:"videoCodec" xml:"videoCodec,attr"`
+				Container       string `json:"container" xml:"container,attr"`
+				VideoFrameRate  string `json:"videoFrameRate" xml:"videoFrameRate,attr"`
+				VideoProfile    string `json:"videoProfile" xml:"videoProfile,attr"`
+				Part            struct {
+					ID           string `json:"id" xml:"id,attr"`
+					Key          string `json:"key" xml:"key,attr"`
+					Duration     string `json:"duration" xml:"duration,attr"`
+					File         string `json:"file" xml:"file,attr"`
+					Size         string `json:"size" xml:"size,attr"`
+					Container    string `json:"container" xml:"container,attr"`
+					VideoProfile string `json:"videoProfile" xml:"videoProfile,attr"`
+					Stream       []struct {
+						ID                 string `json:"id" xml:"id,attr"`
+						StreamType         string `json:"streamType" xml:"streamType,attr"`
+						Default            string `json:"default" xml:"default,attr"`
+						Codec              string `json:"codec" xml:"codec,attr"`
+						Index              string `json:"index" xml:"index,attr"`
+						Bitrate            string `json:"bitrate" xml:"bitrate,attr"`
+						BitDepth           string `json:"bitDepth" xml:"bitDepth,attr"`
+						Cabac              string `json:"cabac" xml:"cabac,attr"`
+						ChromaSubsampling  string `json:"chromaSubsampling" xml:"chromaSubsampling,attr"`
+						CodecID            string `json:"codecID" xml:"codecID,attr"`
+						ColorRange         string `json:"colorRange" xml:"colorRange,attr"`
+						ColorSpace         string `json:"colorSpace" xml:"colorSpace,attr"`
+						Duration           string `json:"duration" xml:"duration,attr"`
+						FrameRate          string `json:"frameRate" xml:"frameRate,attr"`
+						FrameRateMode      string `json:"frameRateMode" xml:"frameRateMode,attr"`
+						HasScalingMatrix   string `json:"hasScalingMatrix" xml:"hasScalingMatrix,attr"`
+						HeaderStripping    string `json:"headerStripping" xml:"headerStripping,attr"`
+						Height             string `json:"height" xml:"height,attr"`
+						Level              string `json:"level" xml:"level,attr"`
+						PixelFormat        string `json:"pixelFormat" xml:"pixelFormat,attr"`
+						Profile            string `json:"profile" xml:"profile,attr"`
+						RefFrames          string `json:"refFrames" xml:"refFrames,attr"`
+						ScanType           string `json:"scanType" xml:"scanType,attr"`
+						Width              string `json:"width" xml:"width,attr"`
+						Selected           string `json:"selected" xml:"selected,attr"`
+						Channels           string `json:"channels" xml:"channels,attr"`
+						AudioChannelLayout string `json:"audioChannelLayout" xml:"audioChannelLayout,attr"`
+						BitrateMode        string `json:"bitrateMode" xml:"bitrateMode,attr"`
+						DialogNorm         string `json:"dialogNorm" xml:"dialogNorm,attr"`
+						SamplingRate       string `json:"samplingRate" xml:"samplingRate,attr"`
+					} `json:"stream" xml:"Stream"`
+				} `json:"part" xml:"Part"`
+			} `json:"media" xml:"Media"`
+		}
+	}
+	PlaylistResponse struct {
+		XMLName   xml.Name      `xml:"MediaContainer"`
+		Playlists []PMSPlaylist `xml:"Playlist"`
 	}
 )
--- plex.go
+++ plex.go
@@ -45,13 +45,40 @@ func (p *Plex) GetServers() ([]PMSServer, error) {
 	return response.Servers, nil
 }
 
+func (p *Plex) GetPlaylists() ([]PMSPlaylist, error) {
+	url := fmt.Sprintf("%s/playlists/all?X-Plex-Token=%s", p.URL, p.Token)
+	resp, err := p.get(url)
+	if err != nil {
+		return []PMSPlaylist{}, err
+	}
+	defer resp.Body.Close()
+	response := &PlaylistResponse{}
+	if err := xml.NewDecoder(resp.Body).Decode(response); err != nil {
+		return []PMSPlaylist{}, err
+	}
+	return response.Playlists, nil
+}
+
+func (p *Plex) GetPlaylistInfo(playlistID string) ([]PMSPlaylistInfo, error) {
+	url := fmt.Sprintf("%s/playlists/%s/items/?X-Plex-Token=%s", p.URL, playlistID, p.Token)
+	resp, err := p.get(url)
+	if err != nil {
+		return PMSPlaylistInfo{}, err
+	}
+	response := &PlaylistInfo{}
+	if err := xml.NewDecoder(resp.Body).Decode(respone); err != nil {
+		return PMSPlaylistInfo{}, err
+	}
+	return response, err
+}
+
 func (p *Plex) get(url string) (*http.Response, error) {
 	client := p.HTTPClient
 
-	req, err := http.NewRequest("GET", query, nil)
+	req, err := http.NewRequest("GET", url, nil)
 
-	if reqErr != nil {
-		return &http.Response{}, reqErr
+	if err != nil {
+		return &http.Response{}, err
 	}
 
 	resp, err := client.Do(req)
